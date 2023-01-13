12048d51395aeefba9512a011cca209d75660516
Nail Gafarov <nil20122013@gmail.com>
2023-01-13 17:07:01 +0500

    Added tests

---
+++ test.js
@@ -0,0 +1,46 @@
+const assert = require("node:assert");
+const pwd = require(".");
+
+describe("pwd()", () => {
+	it("Should return empty string when length is 0", () => {
+		assert.equal(pwd(0), "");
+	});
+	it("Should return string with specified length", () => {
+		assert.equal(pwd(12).length, 12);
+	});
+	it("Returned string should contain numbers when \"numbers\" is true", () => {
+		while (!pwd(10, {numbers: true}).match(/[0-9]/));
+		assert.ok(true);
+	});
+	it("Returned string should not contain numbers when \"numbers\" is false", () => {
+		while (pwd(10, {numbers: false}).match(/[0-9]/));
+		assert.ok(true);
+	});
+	it("Returned string should contain symbols when \"symbols\" is true", () => {
+		while (!pwd(10, {symbols: true}).match(/[~!@#$%^&*\(\)_+\{\}\|:"<>\?`\-=\[\]\\;',.\/]/));
+		assert.ok(true);
+	});
+	it("Returned string should not contain symbols when \"symbols\" is false", () => {
+		while (pwd(10, {symbols: false}).match(/[~!@#$%^&*\(\)_+\{\}\|:"<>\?`\-=\[\]\\;',.\/]/));
+		assert.ok(true);
+	});
+	it("Returned string should contain lowercase when \"lowercase\" is true", () => {
+		while (!pwd(10, {lowercase: true}).match(/[a-z]/));
+		assert.ok(true);
+	});
+	it("Returned string should not contain lowercase when \"lowercase\" is false", () => {
+		while (pwd(10, {lowercase: false}).match(/[a-z]/));
+		assert.ok(true);
+	});
+	it("Returned string should contain uppercase when \"uppercase\" is true", () => {
+		while (!pwd(10, {uppercase: true}).match(/[A-Z]/));
+		assert.ok(true);
+	});
+	it("Returned string should not contain uppercase when \"uppercase\" is false", () => {
+		while (pwd(10, {uppercase: false}).match(/[A-Z]/));
+		assert.ok(true);
+	});
+	it("Should throw an error when all flags are false", () => {
+		assert.throws(() => pwd(10, {numbers: false, symbols: false, lowercase: false, uppercase: false}), {message: "At least one option should be set to true"});
+	});
+});
